2a15405fe97df62e39c839a544b9db46a18efb9d
Filipe Sousa <lfilipesousa17@gmail.com>
2025-07-05 00:27:47 -0300

    Add Clicador feature and update UI elements for tracking

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let moedas = 0; //armazena quantidade de moedas
+let moedas = 0; //armazena quantidade de "dinheiros"
 let valorPorClique = 1;
 let custoUpgrade = 10;
 let valorPorSegundo = 0;
 let custoUpgradePassivo = 50;
+let qtdClicadores = 0;
+let custoClicador = 50;
+let ganhoPorClique = 1;
+let ganhoPorSegundo = 0;
 
 const botaoClique = document.getElementById("clique-btn");
 const botaoUpgrade = document.getElementById("upgrade-btn");
@@ -10,7 +14,7 @@ const spanMoedas = document.getElementById("moedas");
 const botaoUpgradePassivo = document.getElementById("upgrade-passivo-btn");
 
 botaoClique.addEventListener("click", () => {
-  moedas += valorPorClique; //"Quando o botão for clicado, adicione moedas de acordo com o valor do clique"
+  moedas += valorPorClique; //Quando o botão for clicado add moedas de acordo com o valor do clique
 
   atualiazUI();
 });
@@ -35,6 +39,18 @@ botaoUpgradePassivo.addEventListener("click", () => {
   }
 });
 
+function comprarClicador() {
+  if (moedas >= custoClicador) {
+    moedas -= custoClicador;
+    qtdClicadores++;
+    ganhoPorClique = qtdClicadores * ganhoPorClique;
+    custoClicador = Mathfloor(custoClicador * 1.5);
+    atualiazUI();
+  } else {
+    alert("Você não tem Dinheiros suficientes para comprar um Clicador!");
+  }
+}
+
 function atualiazUI() {
   spanMoedas.textContent = moedas; // "Atualiza o texto do span com a quantidade de moedas"
 
@@ -48,12 +64,21 @@ function atualiazUI() {
 
   botaoUpgradePassivo.textContent = `[ Upgrade Passivo (+1/s) ] = ${custoUpgradePassivo} Dinheiros`;
 
+  document.getElementById("custo-clicador").textContent = custoClicador;
+  document.getElementById("qtd-clicadores").textContent = qtdClicadores;
+  document.getElementById("ganho-segundo").textContent = ganhoPorSegundo;
+
   spanMoedas.style.color = moedas >= custoUpgrade ? "green" : "red"; // "Muda a cor do texto das moedas para verde se o custo do upgrade for alcançado, vermelho caso contrário"
 }
 
 setInterval(() => {
   moedas += valorPorSegundo; // "A cada segundo, adiciona moedas de acordo com o valor por segundo"
   atualiazUI();
+}, 1000);
+
+setInterval(() => {
+  moedas += ganhoPorSegundo;
+  atualizarUI();
 }, 1000);
 /*  Por que isso esta no final do código?
 Colocar esse trecho na última linha do script ou abaixo de todas as funções evita que ele rode antes das variáveis estarem todas definidas.
